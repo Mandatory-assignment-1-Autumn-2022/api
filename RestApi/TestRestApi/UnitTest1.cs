73e8e44fe30b25beb0307b2711b6daeed495e8aa
Adam Cipkala <ac@propstep.com>
2022-10-04 20:17:01 +0200

    tests

--- RestApi/TestRestApi/UnitTest1.cs
+++ RestApi/TestRestApi/UnitTest1.cs
@@ -1,11 +1,34 @@
+using ClassLibrary;
+using RestApi.Managers;
+
 namespace TestRestApi
 {
     [TestClass]
     public class UnitTest1
     {
+        FootballPlayersManager _manager = new FootballPlayersManager();
+
+       
+
         [TestMethod]
-        public void TestMethod1()
+        public void Test_Add_Success()
         {
+            FootballPlayer player = new FootballPlayer() { Id = 0, Name = "adamaaaa", Age = 45, ShirtNumber = 21 };
+            _manager.Add(player);
+        }
+
+        [TestMethod]
+        public void Test_Add_ThrowsOutOfRangeExeption()
+        {
+            FootballPlayer player = new FootballPlayer() { Id = 0, Name = "adamaaaa", Age = 45, ShirtNumber = 210 };
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _manager.Add(player));
+        }
+
+        [TestMethod]
+        public void Test_GetById_ReturnsNull()
+        {
+            Assert.IsNull(_manager.GetById(99));
         }
     }
 }
